9a78a831be98785092fdec0395227a14b9b9623b
theroyakash <business.theroyakash@outlook.com>
2020-02-29 00:27:31 +0530

    Update PriorityQueue.py

--- Graph/Graph/PriorityQueue.py
+++ Graph/Graph/PriorityQueue.py
@@ -1,28 +1,71 @@
-class PriorityQueue(object): 
-    def __init__(self): 
-        self.queue = []
-  
-    def __str__(self): 
-        return ' '.join([str(i) for i in self.queue]) 
-  
-    # for checking if the queue is empty 
-    def isEmpty(self): 
-        return len(self.queue) == [] 
-  
-    # for inserting an element in the queue 
-    def insert(self, data): 
-        self.queue.append(data) 
-  
-    # for popping an element based on Priority 
-    def delete(self): 
-        try: 
-            max = 0
-            for i in range(len(self.queue)): 
-                if self.queue[i] > self.queue[max]: 
-                    max = i 
-            item = self.queue[max] 
-            del self.queue[max] 
-            return item 
-        except IndexError: 
-            print() 
-            exit()
+import math
+import sys
+
+class PriorityQueue:
+    # Based on Min Heap
+    def __init__(self):
+        self.cur_size = 0
+        self.array = []
+        self.pos = {}  # To store the pos of node in array
+
+    def isEmpty(self):
+        return self.cur_size == 0
+
+    def min_heapify(self, idx):
+        lc = self.left(idx)
+        rc = self.right(idx)
+        if lc < self.cur_size and lc[0] < idx[0]:
+            smallest = lc
+        else:
+            smallest = idx
+        if rc < self.cur_size and rc[0] < smallest[0]:
+            smallest = rc
+        if smallest != idx:
+            self.swap(idx, smallest)
+            self.min_heapify(smallest)
+
+    def insert(self, tup):
+        # Inserts a node into the Priority Queue
+        self.pos[tup[1]] = self.cur_size
+        self.cur_size += 1
+        self.array.append((sys.maxsize, tup[1]))
+        self.decrease_key((sys.maxsize, tup[1]), tup[0])
+
+    def extract_min(self):
+        # Removes and returns the min element at top of priority queue
+        min_node = self.array[0][1]
+        self.array[0] = self.array[self.cur_size - 1]
+        self.cur_size -= 1
+        self.min_heapify(1)
+        del self.pos[min_node]
+        return min_node
+
+    def left(self, i):
+        # returns the index of left child
+        return 2 * i + 1
+
+    def right(self, i):
+        # returns the index of right child
+        return 2 * i + 2
+
+    def par(self, i):
+        # returns the index of parent
+        return math.floor(i / 2)
+
+    def swap(self, i, j):
+        # swaps array elements at indices i and j
+        # update the pos{}
+        self.pos[self.array[i][1]] = j
+        self.pos[self.array[j][1]] = i
+        temp = self.array[i]
+        self.array[i] = self.array[j]
+        self.array[j] = temp
+
+    def decrease_key(self, tup, new_d):
+        idx = self.pos[tup[1]]
+        # assuming the new_d is atmost old_d
+        self.array[idx] = (new_d, tup[1])
+        while idx > 0 and self.array[self.par(idx)][0] > self.array[idx][0]:
+            self.swap(idx, self.par(idx))
+            idx = self.par(idx)
+
