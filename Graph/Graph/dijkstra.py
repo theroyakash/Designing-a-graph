bbe22be3dd7b3b3a15309622689b0ffd83f376c5
theroyakash <business.theroyakash@outlook.com>
2020-02-28 23:54:09 +0530

    Update dijkstra.py

--- Graph/Graph/dijkstra.py
+++ Graph/Graph/dijkstra.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-def dijsktra_path(graph):
+def dijsktra_path(graph, start_from=0):
     
     distances = [160] * graph.vertices
-    
+    distances[start_from] = 0
     return []
